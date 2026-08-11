19195e56f432dfa4f427d44c982d1710ce892d5f
Vox Brain <vox-brain@users.noreply.github.com>
2026-08-11 12:33:48 +0100

    Remove non-local product packages

--- gateway/migrations/0002_capture_delivery_results.sql
+++
@@ -1,5 +0,0 @@
-ALTER TABLE captures ADD COLUMN delivered_at TEXT
-  CHECK (delivered_at IS NULL OR (
-    delivered_at GLOB '????-??-??T??:??:??.???Z'
-    AND julianday(delivered_at) IS NOT NULL
-  ));
